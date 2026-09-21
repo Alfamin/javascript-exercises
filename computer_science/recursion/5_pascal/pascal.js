ec0551de3c2da5eae324a73e8a29fc1a25851047
Alfamin <Alphamg356@gmail.com>
2026-09-22 00:58:01 +0330

    pascal: build each row from the adjacent pairs of the one above

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,13 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) return [1];
+  const prev = pascal(n - 1)
+  const middle = []
+
+  for (let i = 0; i < prev.length - 1; i++) {
+    middle.push(prev[i] + prev[i + 1])
+  }
+  return [1, ...middle, 1]
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
