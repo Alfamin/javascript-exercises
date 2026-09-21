87a29d376fd582c84e99fc26abc2ed4b3bff7602
Alfamin <Alphamg356@gmail.com>
2026-09-21 21:02:47 +0330

    permutations: base case — one ordering of the empty set

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,8 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+if(arr.length == 0){
+    return [[]]
+}
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
