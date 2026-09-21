e05c893d432412d8b7d1e1d55428f3d8c764f13b
Alfamin <Alphamg356@gmail.com>
2026-09-21 18:26:17 +0330

    totalIntegers: count integers in a flat array

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,12 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(array) {
+    let count = 0
+    array.forEach(el => {
+        if(Number.isInteger(el)){
+            count++
+        }
+    });
+    return count
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
